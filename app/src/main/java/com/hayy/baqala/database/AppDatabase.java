43543c35b7dad86adff96766a60e9b3cbcf8c895
Azix11 <abdalaziz0163@gmail.com>
2026-04-27 20:30:36 +0300

    Fix: remove allowMainThreadQueries() to prevent UI thread blocking

--- app/src/main/java/com/hayy/baqala/database/AppDatabase.java
+++ app/src/main/java/com/hayy/baqala/database/AppDatabase.java
@@ -16,15 +16,15 @@ import com.hayy.baqala.database.entities.Store;
 import com.hayy.baqala.database.entities.User;
 
 @Database(
-        entities = {
-                User.class,
-                Store.class,
-                Product.class,
-                CartItem.class,
-                Order.class
-        },
-        version = 2,
-        exportSchema = false
+    entities = {
+        User.class,
+        Store.class,
+        Product.class,
+        CartItem.class,
+        Order.class
+    },
+    version = 2,
+    exportSchema = false
 )
 public abstract class AppDatabase extends RoomDatabase {
 
@@ -40,13 +40,12 @@ public abstract class AppDatabase extends RoomDatabase {
     public static synchronized AppDatabase getInstance(Context context) {
         if (instance == null) {
             instance = Room.databaseBuilder(
-                            context.getApplicationContext(),
-                            AppDatabase.class,
-                            DATABASE_NAME
-                    )
-                    .allowMainThreadQueries()
-                    .fallbackToDestructiveMigration()
-                    .build();
+                context.getApplicationContext(),
+                AppDatabase.class,
+                DATABASE_NAME
+            )
+            .fallbackToDestructiveMigration()
+            .build();
         }
         return instance;
     }
